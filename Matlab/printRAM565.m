6e1f50fe185d72296423849f12193593d1fde512
Niijon <niijon6@gmail.com>
2021-06-18 10:29:44 +0200

    Create printRAM565.m

---
+++ Matlab/printRAM565.m
@@ -0,0 +1,50 @@
+function printRAM565(img,name)
+[r, c, bD] = size(img);
+fid=fopen(name, 'w');
+fprintf(fid,'v2.0 raw\n');
+for i = 1:r
+    for j = 1:c
+        %R
+        if(img(i,j,1)<16)
+            hr = dec2hex(img(i,j,1)/8);
+            
+            if(img(i,j,1)==0)
+                fprintf(fid,'00');
+            else
+                fprintf(fid,'%x', hr);
+            end
+        else
+            fprintf(fid,'%x', img(i,j,1)/8);
+        end
+        
+        %G
+        if(img(i,j,2)<16)
+            hg = dec2hex(img(i,j,2)/8);
+            
+            if(img(i,j,2)==0)
+                fprintf(fid,'00');
+            else
+                fprintf(fid,'%x', hg);
+            end
+        else
+            fprintf(fid,'%x', img(i,j,2)/8);
+        end
+        
+        %B
+        if(img(i,j,3)<16)
+            hb = dec2hex(img(i,j,3)/8);
+            
+            if(img(i,j,3)==0)
+                fprintf(fid,'00\n');
+            else
+                fprintf(fid,'%x\n', hb);
+            end
+            
+        else
+            fprintf(fid,'%x\n', img(i,j,3)/8);
+        end
+    end
+end
+fclose(fid);
+
+end
